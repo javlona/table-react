a4758461230117efb06d38488a948640b8519171
Javlon <59303287+javlona@users.noreply.github.com>
2022-01-03 23:32:33 -0600

    cards added

--- src/components/cards/card.component.jsx
+++ src/components/cards/card.component.jsx
@@ -3,6 +3,7 @@ import './card.css'
 
 export const Card = (props) => (
         <div className="card-container">
+            <img alt="users" src={`https://picsum.photos/300/400?random=${props.users.id}`}></img>
             <p>{props.users.name}</p>
         </div>
     )
