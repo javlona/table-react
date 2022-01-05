1ff5b79e365c29a249eb19a60e7a67ac9cad149f
Javlon <59303287+javlona@users.noreply.github.com>
2022-01-04 18:09:49 -0600

    styling

--- src/components/cards/card.component.jsx
+++ src/components/cards/card.component.jsx
@@ -4,6 +4,14 @@ import './card.css'
 export const Card = (props) => (
         <div className="card-container">
             <img alt="users" src={`https://picsum.photos/300/400?random=${props.users.id}`}></img>
-            <p>{props.users.name}</p>
+            <h3>{props.users.name}</h3>
+            <p>{props.users.email}</p>
+            <p>{props.users.address.city}</p>
+            <p>{props.users.phone}</p>
+            <p>{props.users.website}</p>
+            <div className="action">
+                <p className="delete">delete</p>
+                <p className="edit">edit</p>
+            </div>
         </div>
     )
