7f8d8cd3ff57d53d0431c92f89d7a1e0f9b5062a
Javlon <59303287+javlona@users.noreply.github.com>
2022-01-09 16:44:10 -0600

    ...

---
+++ src/components/addEntry/addEntry.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import './addEntry.css'
+
+export class AddEntry extends Component {
+    constructor() {
+                                                                                                                                                   
+    }
+}
